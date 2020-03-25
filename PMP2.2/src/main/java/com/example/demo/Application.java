94af36c404dc46a7c00eb23b51fbe5057e262ba7
isaibou <62399006+isaibou@users.noreply.github.com>
2020-03-25 17:25:22 +0100

    Commit Customer

--- PMP2.2/src/main/java/com/example/demo/Application.java
+++ PMP2.2/src/main/java/com/example/demo/Application.java
@@ -1,43 +1,36 @@
 package com.example.demo;
 
 import java.util.Date;
-<<<<<<< HEAD
-=======
+//
+//<<<<<<< HEAD
+//=======
 import java.util.List;
->>>>>>> branch 'master' of https://github.com/isaibou/TEST5
+//>>>>>>> branch 'master' of https://github.com/isaibou/TEST5
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import com.example.demo.entities.AssetType;
-<<<<<<< HEAD
+//<<<<<<< HEAD
 import com.example.demo.repository.AssetRepository;
 import com.example.demo.repository.AssetTypeRepository;
-=======
+//=======
 import com.example.demo.entities.Assets;
 
 import com.example.demo.entities.Customer;
 import com.example.demo.repository.AssetRepository;
 import com.example.demo.repository.AssetTypeRepository;
 
->>>>>>> branch 'master' of https://github.com/isaibou/TEST5
+//>>>>>>> branch 'master' of https://github.com/isaibou/TEST5
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		ApplicationContext ctx =  SpringApplication.run(Application.class, args);
-		AssetTypeRepository assetTypeRepository = ctx.getBean(AssetTypeRepository.class);
-		
-		
-		assetTypeRepository.save(new AssetType("AST1", "BRA1", new Date(), new Date(), "FRU1"));
-		assetTypeRepository.save(new AssetType("AST2", "BRA2", new Date(), new Date(), "FRU2"));
-		assetTypeRepository.save(new AssetType("AST3", "BRA3", new Date(), new Date(), "FRU3"));
-		assetTypeRepository.save(new AssetType("AST3", "BRA3", new Date(), new Date(), "FRU4"));
-		
-=======
+//<<<<<<< HEAD
+	
+//=======
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
 		AssetTypeRepository assettypeRepository = ctx.getBean(AssetTypeRepository.class);
 		AssetRepository assetRepository = ctx.getBean(AssetRepository.class);
@@ -68,7 +61,7 @@ public class Application {
 		 
 		 
 		 assettype.forEach(c->System.out.println(c.getName()));
->>>>>>> branch 'master' of https://github.com/isaibou/TEST5
+//>>>>>>> branch 'master' of https://github.com/isaibou/TEST5
 	}
 
 }
--- PMP2.2/src/main/java/com/example/demo/controllers/AssetTypeController.java
+++ PMP2.2/src/main/java/com/example/demo/controllers/AssetTypeController.java
@@ -1,9 +1,8 @@
 package com.example.demo.controllers;
 
-<<<<<<< HEAD
 import java.util.Date;
-=======
->>>>>>> branch 'master' of https://github.com/isaibou/TEST5
+
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,45 +11,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.entities.AssetType;
-<<<<<<< HEAD
+
 import com.example.demo.repository.AssetTypeRepository;
 
 import javassist.expr.NewArray;
-=======
 
 import com.example.demo.repository.AssetTypeRepository;
->>>>>>> branch 'master' of https://github.com/isaibou/TEST5
+
 
 @Controller
-<<<<<<< HEAD
+
 public class AssetTypeController
 {
+
 	@Autowired
-	AssetTypeRepository assetTyperepository;
-=======
-public class AssetTypeController{
-	
-	@Autowired
-	private AssetTypeRepository assettypeRepository;
->>>>>>> branch 'master' of https://github.com/isaibou/TEST5
+	private AssetTypeRepository assetTypeRepository;
+
 	
 	@RequestMapping(value="/assetstype_manage")
-<<<<<<< HEAD
 	public String Assetstype_Manage(Model model, AssetType assetType) {
 		
-	List<AssetType>	ListassetType = assetTyperepository.findAll();
+	List<AssetType>	ListassetType = assetTypeRepository.findAll();
 	model.addAttribute("ListassetType" , ListassetType);
 	model.addAttribute("assetType", new AssetType());
 	
-=======
-	public String assetstype_manage(Model model) {
-		
-		List<AssetType> asstype = assettypeRepository.findAll();
-		model.addAttribute("assetstype",asstype);
-		
->>>>>>> branch 'master' of https://github.com/isaibou/TEST5
-		return "assetstype_manage";
+	return "assetstype_manage";
+	
+
 	}
+		
+	
 	
 
 }

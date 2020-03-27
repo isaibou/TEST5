d8b0826ac10d78dbf02cc94a79cd74fc44dd41cb
benassou <benassou@ABDESSLAM>
2020-03-27 15:23:26 +0100

    NewUpdate

--- PMP2.2/src/main/java/com/example/demo/entities/Frimware.java
+++ PMP2.2/src/main/java/com/example/demo/entities/Frimware.java
@@ -7,37 +7,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 @Entity
 public class Frimware implements Serializable{
 	
-	@Id
-	@GeneratedValue
-	private Integer Frimware_ID;
-	private Date ReleaseDate;
-	private String Description;
 	
-	public Frimware() {
-		super();
+	public String gFrimwareName() {
+		return FrimwareName;
+	}
+
+
+	public void setFrimwareName(String frimwareName) {
+		FrimwareName = frimwareName;
 	}
 	
-
-	public Frimware(Date releaseDate, String description) {
-		super();
-		ReleaseDate = releaseDate;
-		Description = description;
-	}
-
-
-	public Integer getFrimware_ID() {
-		return Frimware_ID;
-	}
-
-
-	public void setFrimware_ID(Integer frimware_ID) {
-		Frimware_ID = frimware_ID;
-	}
-
-
+	
 	public Date getReleaseDate() {
 		return ReleaseDate;
 	}
@@ -56,6 +41,40 @@ public class Frimware implements Serializable{
 	public void setDescription(String description) {
 		Description = description;
 	}
+	
+	
+	@Id
+	@GeneratedValue
+	private Integer Frimware_ID;
+	private String FrimwareName;
+	@DateTimeFormat(pattern = "MM/DD/YYYY")
+	private Date ReleaseDate;
+	private String Description;
+	
+	public Frimware() {
+		super();
+	}
+	
+
+	public Frimware(Date releaseDate, String description, String frimwarename) {
+		super();
+		ReleaseDate = releaseDate;
+		Description = description;
+		FrimwareName = frimwarename;
+	}
+
+
+	public Integer getFrimware_ID() {
+		return Frimware_ID;
+	}
+
+
+	public void setFrimware_ID(Integer frimware_ID) {
+		Frimware_ID = frimware_ID;
+	}
+
+	
+	
 	
 	
 	

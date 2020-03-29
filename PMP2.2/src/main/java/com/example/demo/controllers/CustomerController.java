8ed4d1b2a9a84c2ad417a20fb6b466f09b17ad0b
isaibou <62399006+isaibou@users.noreply.github.com>
2020-03-29 18:59:23 +0100

    Commit update Customer

--- PMP2.2/src/main/java/com/example/demo/controllers/CustomerController.java
+++ PMP2.2/src/main/java/com/example/demo/controllers/CustomerController.java
@@ -3,9 +3,12 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -32,34 +35,44 @@ public class CustomerController {
 	
 	
 	@RequestMapping(value="/SaveCustomer" , method= RequestMethod.POST)
-	private String SaveCustomer(Customer addCust) {
+	private String SaveCustomer(@Valid Customer addCust, BindingResult bindingResult) {
+		
+		addCust.setStatus("Actif");
+	
 		customerrepository.save(addCust);
 		return "redirect:/customer_manage";
+		
+		
 		
 	}
 	
 	@RequestMapping(value ="/updateCustomerform" )
 	private String updateCustomerform( Model model, Integer id ) {
 	Customer	customer = customerrepository.getOne(id);
-		 model.addAttribute("customerUp",customer);
+		 model.addAttribute("customer",customer);
 		 System.out.println(customer.getName());
 		
 			return "updateCustomerForm";
 			
 	}
 	
-	@RequestMapping(value = "/updateCustomer",method= RequestMethod.POST)
+	@RequestMapping(value = "/editCustomer",method= RequestMethod.POST)
 	public String updateCustomer(Model model, Customer cust){
+	cust= 	(Customer) model.getAttribute("customer");
 		customerrepository.save(cust);
 		
 		return "redirect:/customer_manage";
 	}
 
 
-	@RequestMapping(value ="/deleteCustomer" )
-	private String deleteCustomer( Model model, Integer id ) {
+	@RequestMapping(value ="/archiverCustomer" )
+	private String archiverCustomer( Model model, Integer id ) {
 	
-		customerrepository.deleteById(id);
+	Customer Cust = customerrepository.getOne(id);
+	Cust.setStatus("Archived");
+	Cust.setName("Ibrahim");
+	customerrepository.save(Cust);
+	System.out.println(Cust.getStatus());
 		
 			return "redirect:/customer_manage";	}
 

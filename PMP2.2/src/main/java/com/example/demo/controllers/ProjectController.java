90ee3ed2bec083f7254bdb4f8b4897069184b39d
lenovo <lenovo@DESKTOP-9FP6JKO>
2020-03-30 15:57:12 +0100

    prj

--- PMP2.2/src/main/java/com/example/demo/controllers/ProjectController.java
+++ PMP2.2/src/main/java/com/example/demo/controllers/ProjectController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.entities.*;
-import com.example.demo.repository.CustomerRepository;
 import com.example.demo.repository.ProjetRepository;
 
 @Controller

2d1050ddd80ff37348cc7dd7575a7b21c262a8f1
isaibou <62399006+isaibou@users.noreply.github.com>
2020-03-30 18:32:25 +0100

    commit customer

--- PMP2.2/src/main/java/com/example/demo/controllers/SignInController.java
+++ PMP2.2/src/main/java/com/example/demo/controllers/SignInController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SignInController {
 	
-	@RequestMapping(value="/index")
+	@RequestMapping(value="/Login")
 	public String Login() {
-		return "login";
+		return "Login";
 	}
 
 }

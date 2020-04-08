d99da7a34b71296fce69942d8aaea974dd368796
lenovo <lenovo@DESKTOP-9FP6JKO>
2020-04-08 17:08:42 +0100

    date + upload +design detail

--- PMP2.2/src/main/java/com/example/demo/Application.java
+++ PMP2.2/src/main/java/com/example/demo/Application.java
@@ -1,92 +1,28 @@
 package com.example.demo;
 
-import java.util.Date;
-//
-//<<<<<<< HEAD
-//=======
-import java.util.List;
-//>>>>>>> branch 'master' of https://github.com/isaibou/TEST5
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import com.example.demo.storage.StorageProperties;
 import org.springframework.context.ApplicationContext;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
 
-import com.example.demo.entities.AssetType;
-//<<<<<<< HEAD
-import com.example.demo.repository.AssetRepository;
-import com.example.demo.repository.AssetTypeRepository;
-import com.example.demo.repository.FrimwareRepository;
-//=======
-import com.example.demo.entities.Assets;
+import com.example.demo.entities.*;
+import com.example.demo.repository.*;
 
-import com.example.demo.entities.Customer;
-import com.example.demo.entities.Frimware;
-import com.example.demo.repository.AssetRepository;
-import com.example.demo.repository.AssetTypeRepository;
-
-//>>>>>>> branch 'master' of https://github.com/isaibou/TEST5
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
 
 	public static void main(String[] args) {
-//<<<<<<< HEAD
-	
-//=======
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);
-		AssetTypeRepository assettypeRepository = ctx.getBean(AssetTypeRepository.class);
-		AssetRepository assetRepository = ctx.getBean(AssetRepository.class);
-		FrimwareRepository frimwareRepository = ctx.getBean(FrimwareRepository.class);
-		
-
-		 
-		 
-		 frimwareRepository.save(new Frimware(new Date(), "Frimware1", "frimware2","Status1"));
-		 
-		 
-		 
-
-
-		
-// branch 'master' of https://github.com/isaibou/TEST5
-		 assettypeRepository.save(new AssetType("sasa", "ddd", new Date(), new Date(),"sss", "Status1"));
-		
-		 
-		
-		 
-		assetRepository.save(new Assets("asset1", "asset2", "asset3", "asset4", "asset5", new Date(), "asset6", "asset7", "asset8", 0, new Date()));
-		 
-		
-		
-		
-		
-		
-		
-		
-		List<Assets> assets = assetRepository.findAll();
-	
-		 
-		
-		
-		 
-		assetRepository.save(new Assets("asset1", "asset2", "asset3", "asset4", "asset5", new Date(), "asset6", "asset7", "asset8", 0, new Date()));
-		 
-		
-		
-		
-		
-		
-		
-		
-//		List<Assets> assets = assetRepository.findAll();
-		
-//>>>>>>> branch 'master' of https://github.com/isaibou/TEST5
-		
-		 List<AssetType> assettype = assettypeRepository.findAll();
-		 
-		 
-		 //assettype.forEach(c->System.out.println(c.getName()));
-//>>>>>>> branch 'master' of https://github.com/isaibou/TEST5
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);		
+		//AssetTypeRepository assettypeRepository = ctx.getBean(AssetTypeRepository.class);
+		//AssetRepository assetRepository = ctx.getBean(AssetRepository.class);
+		//FrimwareRepository frimwareRepository = ctx.getBean(FrimwareRepository.class);
+		//List<Assets> assets = assetRepository.findAll();
 	}
 
 }
